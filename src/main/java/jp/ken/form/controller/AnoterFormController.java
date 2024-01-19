ac5aedcd388b4f26cb207459c7e7947aa18a3699
takahiro <iketaka.2116810@gmail.com>
2024-01-19 14:11:21 +0900

    池邉さんのサンプルコードです

---
+++ src/main/java/jp/ken/form/controller/AnoterFormController.java
@@ -0,0 +1,28 @@
+package jp.ken.form.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import jp.ken.form.model.FormModel;
+
+@Controller
+public class AnoterFormController {
+
+	@RequestMapping(value = "/anoter", method = RequestMethod.GET)
+	public ModelAndView form () {
+		ModelAndView modelAndView = new ModelAndView("disp");
+		FormModel formModel = new FormModel();
+		modelAndView.addObject("formModel", formModel);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/anoter", method = RequestMethod.POST)
+	public ModelAndView display(@ModelAttribute FormModel formModel) {
+		ModelAndView modelAndView = new ModelAndView("disp");
+		modelAndView.addObject("message",formModel.getName() + "さん、こんにちは。");
+		return modelAndView;
+	}
+}
